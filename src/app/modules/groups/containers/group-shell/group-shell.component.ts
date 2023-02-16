6093c486dcd0de08710fab6d406525defeab2619
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2023-02-16 14:58:29 +0100

    feat: Add slides

--- src/app/modules/groups/containers/group-shell/group-shell.component.ts
+++ src/app/modules/groups/containers/group-shell/group-shell.component.ts
@@ -1,8 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { filter, map, Observable } from 'rxjs';
+import { IonSlides } from '@ionic/angular';
+import { filter, Observable } from 'rxjs';
 import { GroupsCollection } from 'src/app/database/collections/groups.collection';
 import { Group } from 'src/app/database/storage.interface';
+
+enum Slide {
+    Expenses = 'expenses',
+    Transfers = 'transfers',
+    Totals = 'totals',
+}
 
 @Component({
     selector: 'app-group-shell',
@@ -10,6 +18,15 @@ import { Group } from 'src/app/database/storage.interface';
     styleUrls: ['./group-shell.component.scss'],
 })
 export class GroupShellComponent {
+    @ViewChild(IonSlides) ionSlides?: IonSlides;
+
+    readonly Slide = Slide;
+    readonly slides: Slide[] = [Slide.Expenses, Slide.Transfers, Slide.Totals];
+    readonly defaultSlideIndex = 0;
+    readonly slideOpts = {
+        initialSlide: this.defaultSlideIndex,
+    };
+
     groupId = this.route.snapshot.paramMap.get('groupId') || '';
     group$ = this.groupsCol.getGroup(this.groupId).pipe(
         filter((group) => {
@@ -18,11 +35,26 @@ export class GroupShellComponent {
         })
     ) as Observable<Group>;
 
+    control = new FormControl<Slide>(this.slides[this.defaultSlideIndex], {
+        nonNullable: true,
+    });
+
     constructor(
         private router: Router,
         private route: ActivatedRoute,
         private groupsCol: GroupsCollection
-    ) {}
+    ) {
+        this.control.valueChanges.subscribe((activeSlide) => {
+            if (this.ionSlides) {
+                this.ionSlides.slideTo(this.slides.indexOf(activeSlide));
+            }
+        });
+    }
+
+    slideChanged(e: any) {
+        const { activeIndex } = e.target.swiper;
+        this.control.setValue(this.slides[activeIndex], { emitEvent: false });
+    }
 
     private navigateBack() {
         this.router.navigate(['..'], { relativeTo: this.route });
