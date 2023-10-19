f891327a57659a21ffb40888f2f215a74a288fd1
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2023-10-19 21:17:43 +0200

    feat: Add dev and prod environment

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,17 +1,38 @@
 import { CapacitorConfig } from '@capacitor/cli';
+// https://capacitorjs.com/docs/guides/environment-specific-configurations
+let config: CapacitorConfig;
 
-const config: CapacitorConfig = {
+const baseConfig: CapacitorConfig = {
     appId: 'danyavol.moneysplitter',
     appName: 'Money Splitter',
     webDir: 'www',
-    bundledWebRuntime: false
-,
+    bundledWebRuntime: false,
     android: {
        buildOptions: {
           keystorePath: '/Users/daniil/keystores/money-splitter-keystore.jks',
           keystoreAlias: 'money-splitter',
        }
     }
-  };
+};
+
+switch (process.env['NODE_ENV']) {
+    case "dev":
+        config = {
+            ...baseConfig,
+            android: {
+                ...baseConfig.android,
+                flavor: "dev"
+            }
+        };
+        break;
+    default:
+        config = {
+            ...baseConfig,
+            android: {
+                ...baseConfig.android,
+                flavor: "prod"
+            }
+        };
+}
 
 export default config;
