9847649f462612088f37006a832c22b4daf5ab07
Daniil Volosyuk <daniil.volosyuk@gmail.com>
2024-02-11 22:33:37 +0100

    fix: Tweak some styles

--- src/app/core/services/icons-registry.ts
+++ src/app/core/services/icons-registry.ts
@@ -2,20 +2,4 @@ import { addIcons } from 'ionicons';
 
 addIcons({
   'google': 'assets/icons/google.svg',
-  'mail': 'assets/icons/mail.svg',
-  'delete_20': 'assets/icons/20x20/delete.svg',
-  'edit_20': 'assets/icons/20x20/edit.svg',
-  'account_circle': 'assets/icons/24x24/account_circle.svg',
-  'add': 'assets/icons/24x24/add.svg',
-  'arrow_back': 'assets/icons/24x24/arrow_back.svg',
-  'close': 'assets/icons/24x24/close.svg',
-  'delete': 'assets/icons/24x24/delete.svg',
-  'edit': 'assets/icons/24x24/edit.svg',
-  'expand_more': 'assets/icons/24x24/expand_more.svg',
-  'face': 'assets/icons/24x24/face.svg',
-  'group': 'assets/icons/24x24/group.svg',
-  'person_add': 'assets/icons/24x24/person_add.svg',
-  'person': 'assets/icons/24x24/person.svg',
-  'remove': 'assets/icons/24x24/remove.svg',
-  'add_photo_alternative_48': 'assets/icons/48x48/add_photo_alternative.svg',
 });
